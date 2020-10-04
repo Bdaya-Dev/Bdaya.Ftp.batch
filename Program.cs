49e6ae26e447b3bc4d1b4e9a30aed50e32f969c6
Ahmed777Khalil <progeng_ahmed_khaliloutlook.com>
2020-10-04 09:23:27 +0200

    Adding Work

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace Bdaya.Ftp.batch
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The User: ");
+            var ftpuser = Console.ReadLine();
+            Console.WriteLine("Enter The ServerUrl: ");
+            var ftpServerUrl = Console.ReadLine();
+
+            Console.WriteLine("Enter The password: ");
+            var ftppass = Console.ReadLine();
+
+            Console.WriteLine("Enter The DirPath: ");
+            var dirPath = Console.ReadLine();
+
+            BdayaFtpClient ftp = new BdayaFtpClient(ftpuser, ftppass, ftpServerUrl, dirPath);
+            ftp.UploadDirectory();
+            Console.WriteLine("DONE");
+            Console.ReadLine();
+
+        }
+
+
+
+
+
+    }
+}
